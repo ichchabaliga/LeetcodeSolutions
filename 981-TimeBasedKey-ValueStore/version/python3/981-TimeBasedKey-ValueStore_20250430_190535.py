bfbe1991cd3929177f4b19b80d14bdf19b41c0b0
Ichcha Baliga <42496215+ichchabaliga@users.noreply.github.com>
2025-04-30 19:05:35 -0400

    Create file

---
+++ 981-TimeBasedKey-ValueStore/version/python3/981-TimeBasedKey-ValueStore_20250430_190535.py
@@ -0,0 +1,29 @@
+# Last updated: 30/04/2025, 19:05:35
+# bisect right
+from bisect import bisect_right
+
+class TimeMap:
+    def __init__(self):
+        self.keyStore = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:
+            self.keyStore[key] = []
+        self.keyStore[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.keyStore:
+            return ""
+        values = self.keyStore[key]
+        i = bisect_right(values, (timestamp, chr(127)))
+        if i == 0:
+            return ""
+        return values[i-1][1]
+
+
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
