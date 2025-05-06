4e9387996676eeaa7cfe48023c1ebce352f7ed66
Ichcha Baliga <42496215+ichchabaliga@users.noreply.github.com>
2025-05-05 20:06:55 -0400

    Create file

---
+++ 20-ValidParentheses/version/python3/20-ValidParentheses_20250505_200655.py
@@ -0,0 +1,21 @@
+# Last updated: 05/05/2025, 20:06:55
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s:
+            return True
+        mapBrackets={'}':"{",']':'[',")":"("}
+        stack=[]
+        for ch in s:
+            if ch in "({[":
+                stack.append(ch)
+            else:
+                if  stack and stack[-1]== mapBrackets[ch]:
+                    stack.pop()
+                else:
+                    return False
+        
+        if not stack:
+            return True
+        return False
+
+        
