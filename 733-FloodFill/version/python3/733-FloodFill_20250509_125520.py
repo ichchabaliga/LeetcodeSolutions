de83cbd4bc4a8c7afbbd158841a7757a6d89fc62
Ichcha Baliga <42496215+ichchabaliga@users.noreply.github.com>
2025-05-09 12:55:20 -0400

    Create file

---
+++ 733-FloodFill/version/python3/733-FloodFill_20250509_125520.py
@@ -0,0 +1,18 @@
+# Last updated: 09/05/2025, 12:55:20
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        initialcolor=image[sr][sc]
+        ROWS,COLS=len(image),len(image[0])
+        if initialcolor == color:
+            return image
+        def dfs(r,c):
+            if r<0 or c<0 or r>=ROWS or c>=COLS or image[r][c]!=initialcolor:
+                return 
+            image[r][c]=color
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        dfs(sr,sc)
+        return image
+        
