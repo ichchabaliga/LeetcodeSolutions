ce49d4ff55b509a2f45df2500ed4e91efa692a36
Ichcha Baliga <42496215+ichchabaliga@users.noreply.github.com>
2025-05-09 10:30:32 -0400

    Create file

---
+++ 121-BestTimetoBuyandSellStock/version/python3/121-BestTimetoBuyandSellStock_20250509_103032.py
@@ -0,0 +1,17 @@
+# Last updated: 09/05/2025, 10:30:32
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        low,high=0,1
+        maxProfit=0
+        while high<len(prices):
+            if prices[low]<prices[high]:
+                maxProfit=max(maxProfit,(prices[high]-prices[low]))
+              
+            else:
+                low=high
+            high+=1
+        return maxProfit
+            
+            
+
+        
