d5be69c2b2b6c54ffb37c1d411672f9d44b0c254
Ichcha Baliga <42496215+ichchabaliga@users.noreply.github.com>
2025-05-20 20:02:14 -0400

    Create file

---
+++ 997-FindtheTownJudge/version/python3/997-FindtheTownJudge_20250520_200214.py
@@ -0,0 +1,15 @@
+# Last updated: 20/05/2025, 20:02:14
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+       
+        if len(trust)<n-1:
+            return -1
+        trust_score=[0]*(n+1)
+        for i,j in trust:
+            trust_score[i]-=1
+            trust_score[j]+=1
+        for i,score in enumerate(trust_score[1:],1):
+            if score==n-1:
+                return i
+        return -1 
+        
